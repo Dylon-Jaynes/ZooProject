4d38365d8b19659d7a6eb3d8ff8e0ea0aea12a5e
wil16188 <45651632+wil16188@users.noreply.github.com>
2019-12-14 14:31:01 -0700

    Update Lion.java

--- src/Lion.java
+++ src/Lion.java
@@ -1,2 +1,15 @@
+/**
+ * Program to enter the information for a new lion
+ */
+
 public class Lion {
+
+    //Print a description for this task
+
+    //Prompt the user for the lion's name, sex, and habitat
+
+    //Store the data in an animal object
+    
+    //Add a reference to the animal in an array of animals
+
 }
