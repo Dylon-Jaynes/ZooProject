32723d9f8a25435292dd545e98d17d4f3e4f9f8c
wil16188 <45651632+wil16188@users.noreply.github.com>
2019-12-14 15:09:27 -0700

    Update Bird.java

--- src/Bird.java
+++ src/Bird.java
@@ -5,6 +5,13 @@
 public class Bird {
 
     //Print a description for this task
+    
+    /**
+    * Bird Constuctor
+    **/
+    public Bird(String name, String sex, String habitat, String color, String fly) {
+        birdCount++;
+    }
 
     //Prompt the user for the bird's name, sex, habitat, color, and if it can fly
 
