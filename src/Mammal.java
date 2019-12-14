c4c7b30df697d3202a05d810229b44fa87fa1e0e
wil16188 <45651632+wil16188@users.noreply.github.com>
2019-12-14 14:37:54 -0700

    Update Mammal.java

--- src/Mammal.java
+++ src/Mammal.java
@@ -7,5 +7,7 @@ public class Mammal {
     //Print a description for this task
 
     //Prompt the user for the mammal's species (Giraffe, Lion, or Bear)
+    
+    //Use specified species class to gather further information
 
 }
