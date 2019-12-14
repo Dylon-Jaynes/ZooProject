ef194415bc2c4bcf42368bf5a8b8d8b8f1edec36
wil16188 <45651632+wil16188@users.noreply.github.com>
2019-12-14 14:35:34 -0700

    Update Mammal.java

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,2 +1,11 @@
+/**
+ * Program to choose the species of a new mammal
+ */
+
 public class Mammal {
+
+    //Print a description for this task
+
+    //Prompt the user for the mammal's species (Giraffe, Lion, or Bear)
+
 }
