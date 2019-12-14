3c7ef0e330a00a51e0f1c059cb136de8338bfb35
wil16188 <45651632+wil16188@users.noreply.github.com>
2019-12-14 14:43:18 -0700

    Update Mammal.java

--- src/Mammal.java
+++ src/Mammal.java
@@ -5,9 +5,23 @@
 public class Mammal {
 
     //Print a description for this task
+    System.out.println(
+                "Please select the species of mammal you would like to add to the zoo.");
+    System.out.println();
 
     //Prompt the user for the mammal's species (Giraffe, Lion, or Bear)
+    System.out.print("Enter 0 for Giraffe, 1 for Lion, or 2 for Bear: ");
     
-    //Use specified species class to gather further information
+    //Check for valid species number entry
+    if (dayNumber < 0) {
+            System.err.format("%d is invalid. You must enter a number between 0 and 2. %n", dayNumber);
+            System.exit(0);
+        }
+        else if (dayNumber > 2) {
+            System.err.format("%d is invalid. You must enter a number between 0 and 2. %n", dayNumber);
+            System.exit(0);
+        }
+    
+    //Use number entered to direct the user to the specified species class to gather further information
 
 }
